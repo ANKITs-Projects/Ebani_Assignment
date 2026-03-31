df5534de1166ddc0aa21e6b93b5f4b87ab84faf3
ANKITs-Projects <ankitgupta0429@gmail.com>
2026-03-31 14:41:11 +0530

    Ebani_Assignment done

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,17 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const connectDB = async () => {
-    try {
-        // Options to help with stability
-        await mongoose.connect(process.env.MONGO_DB_URI, {
-            serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 10s
-        });
-        console.log("DB Connection Successful");
-    } catch (err) {
-        console.error(`DB connection failed: ${err.message}`);
-        // Exit process with failure if you can't connect
-        process.exit(1);
-    }
-};
+mongoose.connect(process.env.MONGO_DB_URI)
 
-connectDB();
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log("DB Connection Successful")
+})
+
+db.on('error', (err) => {
+    console.log(`db connection failed : ${err.message}`)
+})
