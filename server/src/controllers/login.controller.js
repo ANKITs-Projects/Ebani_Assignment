43492bcf0f146ca4df088e7ba2527593453e3ca9
ANKITs-Projects <ankitgupta0429@gmail.com>
2026-04-01 10:41:55 +0530

    vercel setup

--- server/src/controllers/login.controller.js
+++ server/src/controllers/login.controller.js
@@ -1,27 +1,29 @@
-
-
 class LoginController {
-    constructor(loginService){
-        this.loginService = loginService
+  constructor(loginService) {
+    this.loginService = loginService;
+  }
+
+  login = async (req, res) => {
+    try {
+      const data = await this.loginService.login(req.body);
+
+      // 🚫 Stop Vercel + browser from caching the response
+      res.set("Cache-Control", "no-store");
+      res
+        .cookie("token", data.token)
+        .status(200)
+        .json({
+          success: true,
+          message: `${data.userdata.role} login successfuly`,
+          user: data.userdata,
+        });
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
     }
-
-    login = async (req, res) => {
-        try {
-            const data = await this.loginService.login(req.body)
-
-            res.cookie("token", data.token).status(200).json({
-                success: true,
-                message: `${data.userdata.role} login successfuly`,
-                user: data.userdata
-            })
-        } catch (error) {
-            res.status(400).json({
-                success: false,
-                message: error.message
-            })
-        }
-    }
-
+  };
 }
 
-module.exports = LoginController
+module.exports = LoginController;
